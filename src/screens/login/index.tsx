c5a5694443452451701b0e37f160c79ca218199f
xiaolong666666 <long.1585097369@qq.com>
2022-07-07 23:11:47 +0800

    feat: test

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -3,6 +3,7 @@ import { useAuth } from "context/auth-provider";
 
 const LoginScreen = () => {
   const { user, register, login, logout } = useAuth();
+  console.log("test cwl");
 
   const onHandleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
